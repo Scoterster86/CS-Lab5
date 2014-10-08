8b8e35afea7be81ba13510141273dafdc323cd19
ykarita <ykarita@wpi.edu>
2014-10-08 12:52:32 -0400

    Update main.cpp

--- lab5/lab5/main.cpp
+++ lab5/lab5/main.cpp
@@ -4,6 +4,25 @@
 
 int main(int argc, char* argv[])
 {
-	
+	int n = atoi(argv[1]);
+	TimeSlice = atoi(argv[2]);
+
+	Shopper* shopper = new Shopper();	//create a shopper
+
+	int xpos=0, ypos=0, zpos=1, t = 0;
+
+	for(int i = 0; i < n ; i++)
+	{
+		cin >> xpos, ypos, t;
+		Store* store = new Store();		//create a store
+		store->xstore_ = xpos;	// sets the location
+		store->ystore_ = ypos;
+		store->zstore_ = 1;	//sets 1 for lab5
+		store->TimeSlice_ = t;	//sets the timeslice
+
+		shopper->AddStore(store);	//Add the store to the shopper's storelist
+	}
+
+	shopper->BotMove();
 	return 0;
 }
