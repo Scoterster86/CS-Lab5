c6573adc64c8976e3f6a67449b586659214f2800
Scoterster86 <swiwanicki@gmail.com>
2014-10-08 20:30:55 -0700

    Update rrqueue.cpp

--- newlab5/lab5/rrqueue.cpp
+++ newlab5/lab5/rrqueue.cpp
@@ -19,3 +19,25 @@ void RRqueue::AddShopper(Shopper* shopper)
 	}
   
 }
+
+void RemoveShopper(){
+	this->headptr_ = this->headptr_->nextshopper_;
+}
+void RunQueue()
+{
+	Shopper* = ptr;
+	ptr = this->headptr_;
+	RemoveShopper();
+	if(ptr->storelist_->TimeSlice_ > TimeSlice)
+	{
+		ptr->storelist_->TimeSlice_ -= TimeSlice;
+		ptr->slices++;
+		this->AddShopper(ptr);
+	}
+	else
+	{
+		ptr->slices++;
+		cout<<"Shopper 1 leaves store (" <<ptr->xpos <<", " << ptr->ypos <<") after " << ptr->slices << "Time slices at Time" <<TIME endl;
+		
+	}
+}
