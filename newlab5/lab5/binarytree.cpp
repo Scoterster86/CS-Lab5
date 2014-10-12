5aff6c0a87909a68c72f946f58a2ecc263b3b501
Scoterster86 <swiwanicki@gmail.com>
2014-10-12 16:07:29 -0700

    Create binarytree.cpp

---
+++ newlab5/lab5/binarytree.cpp
@@ -0,0 +1,89 @@
+//Scott Iwanicki
+//default constructor for a binary tree
+BinaryTree::BinaryTree()
+{
+  this->node_ = Item();
+  this->left_ = nullptr;
+  this->right_ = nullptr;
+}
+
+//Scott Iwanicki
+//default constructor for a binary tree with an item
+BinaryTree::BinaryTree(Item item)
+{
+  this->node_ = item;
+  this->left_ = nullptr;
+  this->right_ = nullptr;
+}
+
+
+//Scott Iwanicki
+//adds an item to the binary tree
+void BinaryTree::AddItem(Item* item, Store* store)
+{
+
+  /// if there is no items
+  if (this->node_.ProductCode_ == "")
+    {
+      this->node_ = *item;
+      this->node_.pStore = store;
+      //this->node_.pStore->AddStore(store);
+      return;
+    }
+
+  /// if node has same productcode
+  if (this->node_.HasPriority(*item) == 0)
+    {
+
+      this->node_.pStore->AddStore(store);
+      this->node_.pStore = headstoreptr;
+    }
+  /// if the node has higher priority,
+  else if (this->node_.HasPriority(*item) == 1)
+    {
+      if(this->left_) this->left_->AddItem(item, store);
+      else
+        {
+          BinaryTree* newTree = new BinaryTree(*item);
+          this->left_ = newTree;
+          this->left_->node_.pStore = store;
+          //this->left_->node_.pStore->AddStore(store);
+        }
+
+    }
+
+  /// if the node has lower priority,
+  else if (this->node_.HasPriority(*item) == -1)
+    {
+      if (this->right_){
+         this->right_->AddItem(item, store);   
+      }
+      else{
+          BinaryTree* newTree = new BinaryTree(*item);
+          this->right_ = newTree;
+          this->right_->node_.pStore = store;
+          //this->right_->node_.pStore->AddStore(store);
+       }
+    }
+}
+
+//Scott Iwanicki
+//prints out the binary tree
+void BinaryTree::PrintBinaryTree()
+{
+  //exits the function when this is NULL
+  if (this == nullptr) return;
+
+  //prints out the left branch of the binary tree
+  this->left_->PrintBinaryTree();
+
+  //prints out the current node
+  cout << this->node_.ProductCode_ << ": ";
+
+  //prints out the current node's list of stores
+  this->node_.pStore->PrintStores();
+
+  //prints out the right branch of the binary tree
+  this->right_->PrintBinaryTree();
+}
+
