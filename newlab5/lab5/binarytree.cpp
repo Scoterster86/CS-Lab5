4308c2f8824fc47274ceca281136ba1a87e1532b
Scoterster86 <swiwanicki@gmail.com>
2014-10-13 05:50:32 -0700

    Update binarytree.cpp

--- newlab5/lab5/binarytree.cpp
+++ newlab5/lab5/binarytree.cpp
@@ -3,7 +3,7 @@
 //default constructor for a binary tree
 BinaryTree::BinaryTree()
 {
-  this->node_ = Item();
+  this->node_ = new Item();
   this->left_ = nullptr;
   this->right_ = nullptr;
 }
@@ -12,7 +12,7 @@ BinaryTree::BinaryTree()
 //default constructor for a binary tree with an item
 BinaryTree::BinaryTree(Item item)
 {
-  this->node_ = item;
+  this->node_ = &item;
   this->left_ = nullptr;
   this->right_ = nullptr;
 }
@@ -24,10 +24,10 @@ void BinaryTree::AddItem(Item* item, Store* store, Store* head)
 {
 
   /// if there is no items
-  if (this->node_.ProductCode_ == "")
+  if (this->node_->ProductCode_ == "")
     {
       this->node_ = item;
-      this->node_.pStore = store;
+      this->node_->pStore = store;
       //this->node_.pStore->AddStore(store);
       return;
     }
@@ -36,8 +36,8 @@ void BinaryTree::AddItem(Item* item, Store* store, Store* head)
   if (this->node_.HasPriority(*item) == 0)
     {
     cout<<"BT line 38" <<endl;
-      this->node_.pStore->AddStore(store, head);
-      this->node_.pStore = head;
+      this->node_->pStore->AddStore(store, head);
+      this->node_->pStore = head;
     }
     
   /// if the node has higher priority,
@@ -48,7 +48,7 @@ void BinaryTree::AddItem(Item* item, Store* store, Store* head)
         {
           BinaryTree* newTree = new BinaryTree(*item);
           this->left_ = newTree;
-          this->left_->node_.pStore = store;
+          this->left_->node_->pStore = store;
           //this->left_->node_.pStore->AddStore(store);
         }
 
@@ -81,10 +81,10 @@ void BinaryTree::PrintBinaryTree()
   this->left_->PrintBinaryTree();
 
   //prints out the current node
-  cout << this->node_.ProductCode_ << ": ";
+  cout << this->node_->ProductCode_ << ": ";
 
   //prints out the current node's list of stores
-  this->node_.pStore->PrintStores();
+  this->node_->pStore->PrintStores();
 
   //prints out the right branch of the binary tree
   this->right_->PrintBinaryTree();
