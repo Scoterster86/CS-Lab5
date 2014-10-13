df134a5310924a2124fc5a4797f8d7c13cb1137b
Scoterster86 <swiwanicki@gmail.com>
2014-10-13 09:21:59 -0700

    Update lab5.h

--- newlab5/lab5/lab5.h
+++ newlab5/lab5/lab5.h
@@ -124,7 +124,7 @@ public:
 	
 	BinaryTree(Item*);
 	void AddItem(Item*, Store*);
-	AddItem(Item*, Store*, Store*);
+	void AddItem(Item*, Store*, Store*);
 	void PrintBinaryTree();
 	void AddLeft(Item);
 	void AddRight(Item);
