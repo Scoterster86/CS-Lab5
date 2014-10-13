b293404e6cfa6aff14e01c3416c2b9f6eae8ca84
Scoterster86 <swiwanicki@gmail.com>
2014-10-13 06:16:25 -0700

    Update binarytree.cpp

--- newlab5/lab5/binarytree.cpp
+++ newlab5/lab5/binarytree.cpp
@@ -75,6 +75,7 @@ void BinaryTree::AddItem(Item* item, Store* store, Store* head)
 //prints out the binary tree
 void BinaryTree::PrintBinaryTree()
 {
+  cout<<"Enters printBT" <<endl;
   //exits the function when this is NULL
   if (this == nullptr) return;
 
