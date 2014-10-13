f4a676e9765c787a242db7b99e20959e2e6eb71b
Scoterster86 <swiwanicki@gmail.com>
2014-10-13 09:44:28 -0700

    Update store.cpp

--- newlab5/lab5/store.cpp
+++ newlab5/lab5/store.cpp
@@ -1,46 +1,146 @@
 #include "lab5.h"
 
+//Yo Karita
+//constructor for Store with a store as an input
 Store::Store(Store* store)
 {
-	this->xstore_ = store->xstore_;
-	this->ystore_ = store->ystore_;
-	this->zstore_ = store->zstore_;
-	this->TimeSlice_ = store->TimeSlice_;
-	this->pNext = nullptr;
+        this->xstore_ = store->xstore_;
+        this->ystore_ = store->ystore_;
+        this->zstore_ = store->zstore_;
+        this->productamount_ = store->productamount_;
+        this->TimeSlice_ = store->TimeSlice_;
+        this->items_= NULL;
+        this->pNext = nullptr;
 }
-
-
+//Yo Karita
+//constructor for a store
 Store::Store()
 {
-	this->xstore_ = 0;
-	this->ystore_ = 0;
-	this->zstore_ = 0;
-	this->TimeSlice_ = 0;
-	this->pNext = nullptr;
+        this->xstore_ = 0;
+        this->ystore_ = 0;
+        this->zstore_ = 0;
+        this->TimeSlice_ = 0;
+        this->productamount_ = 0;
+        this->items_= NULL;
+        this->pNext = nullptr;
 }
 
-///add a store to the storelist 
-void Store::AddStore(Store* store)
+Store::Store(int x, int y, int z)
 {
-	Store* ptr = this;
-	
-	//look for the end of the storelist
-	while (ptr->pNext)
-	{
-		ptr = ptr->pNext;
-	}
-	ptr->pNext = store;	//the end of store has a points to the next store
+        this->xstore_ = x;
+        this->ystore_ = y;
+        this->zstore_ = z;
+        this->TimeSlice_ = 0;
+        this->items_= new Item();
+        this->pNext = nullptr;
 }
 
+//Yo Karita
+//Adds a store to the list of stores
+void Store::AddStorelist(Store* store)
+{
+        Store* ptr = this;
+
+        while (ptr->pNext)
+        {
+                ptr = ptr->pNext;
+        }
+        ptr->pNext = store;
+}
+
+//Yo Karita
+//adds a store to the list of stores
+void Store::AddStore(Store* store, Store* head)
+{
+  //if the store location is (0,0,0) set the first store in the list
+  if (this->xstore_ == 0 && this->xstore_ == 0 && this->zstore_ == 0)
+    {
+      this->xstore_ = store->xstore_;
+      this->ystore_ = store->ystore_;
+      this->zstore_ = store->zstore_;
+
+      this->productamount_ += store->productamount_;
+
+      head = store;
+      return;
+    }
+ //sets the head pointer of the list to the first store in the list
+  head = this;
+
+  //sets the currento pointer to the first store in the list
+  Store* pCurrent = this;
+
+  //sets the previous pointer to null
+  Store* pPrev = nullptr;
+
+  //loops until pCurrent is null or then the current product amount
+  //is greater than the store's prodcut amount or if the stores are the same
+  while (pCurrent && (this->productamount_ > store->productamount_) && (
+        (this->xstore_ =! store->xstore_) ||
+        (this->ystore_ =! store->ystore_)||
+        (this->zstore_=! store->zstore_)) )
+    {
+      //sets the previous pointer to the current pointer
+      pPrev = pCurrent;
+
+      //sets the current pointer to the next store in the list
+      pCurrent = pCurrent->pNext;
+    }
+  //if the store already exsists in the list
+  if((this->xstore_ = store->xstore_) &&
+     (this->ystore_ = store->ystore_)&&
+     (this->zstore_ = store->zstore_))
+    {
+      //add the new product amount to the total product amount
+      this->productamount_ += store->productamount_;
+      return;
+    }
+  //if the previous pointer isn't null instert the store between the previous and current store
+  if (pPrev)
+    {
+      //set the previous store's next pointer to the store
+      pPrev->pNext = store;
+      //set the store's next pointer to the current store
+      store->pNext = pCurrent;
+    }
+  //if the previous pointer is null
+  else
+    {
+      //set the store's next pointer as the previous head of the list
+      store->pNext = this;
+      //set the head pointer of the list to the store
+      head = store;
+    }
+}
+
+//Yo Karita
+//Counts the number of stores in the list
 int Store::GetSize()
 {
+       int n = 0;
+        Store* ptr = this;
+        while (ptr)
+        {
+                n++;
+                ptr = ptr->pNext;
+        }
+        return n;
+}
 
-	int n = 0;
-	Store* ptr = this;
-	while (ptr)	//loop until it reaches to the end of the list
-	{
-		n++;
-		ptr = ptr->pNext;
-	}
-	return n;
+
+//Yo Karita
+//prints out the stores
+void Store::PrintStores()
+{
+  //sets the current pointer to this
+  Store* pCurrent = this;
+
+  //while the current pointer isn't null
+  while (pCurrent)
+    {
+      //print out the store location and the product amount
+      cout << "(" << pCurrent->xstore_ << ", " << pCurrent->ystore_ << ", " <<\
+ pCurrent->zstore_ << ")Total items: " << pCurrent->productamount_ << "   ";
+      pCurrent = pCurrent->pNext;
+    }
 }
