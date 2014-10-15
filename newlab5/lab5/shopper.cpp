386f2a99159b43ba410e104f961756a8cf5bd7a3
Scoterster86 <swiwanicki@gmail.com>
2014-10-15 16:56:41 -0700

    Update shopper.cpp

--- newlab5/lab5/shopper.cpp
+++ newlab5/lab5/shopper.cpp
@@ -49,11 +49,9 @@ int Shopper::storecomplete(){
 //adds a shopper to a list of shoppers
 void Shopper::AddShopper(Shopper* shopper)
 {
-	cout<<"adding shopper"<<endl;
         //if the first shopper is empty, set the shopper as the head shopper
         if (this->RobotNum_ == 0)
         {
-        	cout<<"empy list"<<endl;
                 this->RobotNum_ = shopper->RobotNum_;
                 this->smove_->xpos_ = shopper->smove_->xpos_;
                 this->smove_->ypos_ = shopper->smove_->ypos_;
