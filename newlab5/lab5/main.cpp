0f3bf2eeda28919dac492a9aa1c2fee4ec644a4e
Scoterster86 <swiwanicki@gmail.com>
2014-10-15 13:08:06 -0700

    Update main.cpp

--- newlab5/lab5/main.cpp
+++ newlab5/lab5/main.cpp
@@ -201,7 +201,7 @@ int main(int argc, char* argv[])
     }
     Shoppers->AddShopper(nextShopper);
     Shopper* ptr = Shoppers->listhead_;
-    cout"while loop"<<endl;
+    cout<<"while loop"<<endl;
     while(ptr){
           cout<<ptr->RobotNum_ << endl;
           ptr= ptr->nextshopper_;
