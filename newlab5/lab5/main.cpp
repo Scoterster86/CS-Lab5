7de372d346243051a812cac5ecb817762c218920
Scoterster86 <swiwanicki@gmail.com>
2014-10-15 21:24:20 -0700

    Update main.cpp

--- newlab5/lab5/main.cpp
+++ newlab5/lab5/main.cpp
@@ -426,9 +426,7 @@ int main(int argc, char* argv[])
 								//if the shopper is at the head of the store
 								if(Debug)cout<<"shopper in queue"<<endl <<currentStore->storequeue->listhead_->RobotNum_<<endl;
 								if(currentShopperptr->RobotNum_  == currentStore->storequeue->listhead_->RobotNum_ ){
-									if(currentShopperptr->smove_->storelist_->zstore_ == 0){
-										currentStore->storequeue->RunQueue();
-									}
+									currentStore->storequeue->RunQueue();
 								}
 							}
 							else{
