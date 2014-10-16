14046f16985f33c1f958641b22477d1a29eec6f8
Scoterster86 <swiwanicki@gmail.com>
2014-10-15 20:24:35 -0700

    Update queue.cpp

--- newlab5/lab5/queue.cpp
+++ newlab5/lab5/queue.cpp
@@ -15,7 +15,7 @@ void Queue::RemoveShopper()
 ///Get a shopper pointer and put it at the end of the list
 void Queue::AddShopper(Shopper* shopper)
 {
-	if(this == NULL)cout<<"Adding Shopper"<<endl;
+	if(this == NULL){
 	if(this->listhead_ != NULL){
 	//cout<<"Adding Shopper to end of list"<<endl;
 	this->listtail_->nextshopper_= shopper;
