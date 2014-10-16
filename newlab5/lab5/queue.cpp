07809bd7b6ce841bd6208a7c10ceb32a642cf7a3
Scoterster86 <swiwanicki@gmail.com>
2014-10-15 19:15:23 -0700

    Update queue.cpp

--- newlab5/lab5/queue.cpp
+++ newlab5/lab5/queue.cpp
@@ -26,3 +26,43 @@ void Queue::AddShopper(Shopper* shopper)
 		this->listtail_=shopper;
 	}
 }
+//Scott Iwanicki
+//if the shopper is in a store on the first floor then use the fcfs queue else
+//processes the head of the list and removes it from the queue if its service is complete,
+//or inserts the shopper at the end of the queue
+void Queue::RunQueue()
+{
+        Shopper* ptr;
+        ptr = this->listhead_;
+        //if the shopper needs more service
+        ptr->smove_->storelist_->items_->itemsleft_ --;
+        if(ptr->smove_->storelist_->zstore_ == 0){
+        	if(ptr->smove_->storelist_->items_->itemsleft_==0){
+        			ptr->smove_->storelist_->RemoveItem();
+        			if(ptr->smove_->storelist_->items_ == NULL){
+        				this->listhead_ = this->listhead_->nextshopper_;
+        			}
+        	}
+        }
+        else{
+        	if(ptr->smove_->storelist_->items_->amount_ - ptr->smove_->storelist_->items_->itemsleft_ % TimeSlice = 0 ||
+        			ptr->smove_->storelist_->items_->itemsleft_==0){
+        		if(ptr->smove_->storelist_->items_->itemsleft_ != 0){
+        			this->listtail_->nextshopper_ = this->listhead_;
+        			this->listtail_ = this->listhead_;
+        			this->listhead_ = this->listhead_->nextshopper_;
+        		}
+        		else{
+        			ptr->smove_->storelist_->RemoveItem();
+        			if(ptr->smove_->storelist_->items_ == NULL){
+        				this->listhead_ = this->listhead_->nextshopper_;
+        			}
+        			else{
+					this->listtail_->nextshopper_ = this->listhead_;
+        				this->listtail_ = this->listhead_;
+        				this->listhead_ = this->listhead_->nextshopper_;
+        			}
+        		}
+        	}
+        }
+}
