e6598351d6363a2e3f91aad390e3dc7a87088971
Scoterster86 <swiwanicki@gmail.com>
2014-10-15 18:04:21 -0700

    Update store.cpp

--- newlab5/lab5/store.cpp
+++ newlab5/lab5/store.cpp
@@ -207,7 +207,7 @@ Store* Store::FindStore(Store* store){
 
 int Store::ShopperinQueue(Shopper* shopper){
 	Shopper* shopperlist = this->storequeue->listhead_;
-	while(shopperlist && (shopperlist->RobotNum_ != shopper->RobotNum_))){
+	while(shopperlist && (shopperlist->RobotNum_ != shopper->RobotNum_)){
 		shopperlist = shopperlist->pNext;
 	}
 	if(shoperlist){
