2275380b065cb89134994ecf3618a94b4baaae68
Scoterster86 <swiwanicki@gmail.com>
2014-10-16 09:19:02 -0700

    Update main.cpp

--- newlab5/lab5/main.cpp
+++ newlab5/lab5/main.cpp
@@ -444,7 +444,7 @@ int main(int argc, char* argv[])
 								  currentStore->storequeue->listhead_->smove_->ypos_ != currentStore->ystore_ ||
 								  currentStore->storequeue->listhead_->smove_->zpos_ != currentStore->zstore_ ){
 								  	cout<<"REMOVE HEAD SHOPPER NEW HEAD "<<currentStore->storequeue->listhead_->nextInQueue_->RobotNum_<<endl;
-								  	currentStore->storequeue->listhead_ = currentStore->storequeue->listhead_->nextInQueue_;
+								  	currentStore->storequeue->listhead_ = NULL;
 								  }
 								//if the shopper is at the head of the store
 								if(currentShopperptr->RobotNum_  == currentStore->storequeue->listhead_->RobotNum_ ){
