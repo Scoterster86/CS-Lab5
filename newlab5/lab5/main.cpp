487fb2215eed5a4038c77cffe2cd24a80ab15101
Scoterster86 <swiwanicki@gmail.com>
2014-10-16 09:14:24 -0700

    Update main.cpp

--- newlab5/lab5/main.cpp
+++ newlab5/lab5/main.cpp
@@ -443,7 +443,7 @@ int main(int argc, char* argv[])
 								if(currentStore->storequeue->listhead_->smove_->xpos_ != currentStore->xstore_ ||
 								  currentStore->storequeue->listhead_->smove_->ypos_ != currentStore->ystore_ ||
 								  currentStore->storequeue->listhead_->smove_->zpos_ != currentStore->zstore_ ){
-								  	cout"REMOVE HEAD SHOPPER"<<endl;
+								  	cout<<"REMOVE HEAD SHOPPER"<<endl;
 								  	currentStore->storequeue->listhead_ = currentStore->storequeue->listhead_->nextInQueue_;
 								  }
 								//if the shopper is at the head of the store
